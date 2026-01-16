59ebbc48d26137f73dfa3f7617ca02cb009491c3
Aman8076 <amanadv2022@gmail.com>
2026-01-16 16:00:25 +0000

    converesion done

---
+++ java_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let userName="aman"
+let age=19
+let isLoggedIn=false
+console.log(userName);
+console.log(typeof userName)
+console.log(typeof(userName))
+console.log(typeof "aman")
+console.log((typeof null))
+console.log((typeof undefined))
+
---
+++ java_basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+let score="33"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn=1;
+let bolleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(bolleanIsLoggedIn)
+
+let iLoggedIn="";
+let booleanIsLoggedIn=Boolean(iLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let valueIs=null;
+let booleanvalueIs=Boolean(valueIs)
+console.log(booleanvalueIs)
