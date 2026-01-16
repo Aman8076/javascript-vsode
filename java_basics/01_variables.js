45bf9d2358e1854dcdcc49b249b2c5c02b65d2bc
Aman8076 <amanadv2022@gmail.com>
2026-01-16 12:07:14 +0000

    file variable

---
+++ java_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=14456
+let accountEmail="aman@gmail.com"
+var accountPassword="233434"
+accountCity="faridabad"
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
